23669ca56984f8b9b49e8e089e00e577371dbe1f
KARAN SHANKAR TRIPATHI <karanboss652@gmail.com>
2023-07-27 19:29:07 +0530

    push

--- Anime Recommendation/anime.py
+++ Anime Recommendation/anime.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import MaxAbsScaler
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 app = Flask(__name__, template_folder='template')
 
@@ -22,7 +24,7 @@ X = pd.concat([genre_, type_, episode_, df['rating'], df['members']], axis=1)
 X.head(10)
 scaled = MaxAbsScaler()
 X = scaled.fit_transform(X)
-recommendations = NearestNeighbors(n_neighbors=11).fit(X)
+recommendations = NearestNeighbors(n_neighbors=10).fit(X)
 recommendations.kneighbors(X)
 anime_indices = recommendations.kneighbors(X)[1]
 
@@ -54,6 +56,35 @@ def recommend_me(anime):
              'rating': df.iloc[i]['rating']}
         )
     return recommendations
+
+@app.route('/plots')
+def ploting():
+  
+    plt.figure(figsize=(15, 7))
+    plt.ylim(0.0, 10.0)
+    plt.bar(df['name'].head(5), df['rating'].head(5))
+    plt.xlabel("Popularity")
+    plt.title("Popular anime")
+    plt.savefig('static/barchart.jpg')
+
+    genre_member = df.groupby("genre", as_index=False)["members"].sum()
+    genre_member.sort_values(by="members", ascending=False, inplace=True)
+    genre_member.head(3)
+
+    plt.figure()
+    g = sns.scatterplot(data=df, x="members", y="rating", hue="type")
+    g.set_title("Rating vs Members by Type")
+    g.set_xlabel("Members")
+    g.set_ylabel("Rating")
+    plt.savefig('static/scatterplot.jpg')
+
+    types = pd.DataFrame(df["type"].value_counts())
+    types.plot.pie(y="type", figsize=(8, 8), autopct="%1.1f%%")
+    plt.legend()
+    plt.title("Distribution of Anime Type")
+    plt.savefig('static/piechart.jpg')
+
+    return render_template('plots.html')
 
 
 if __name__ == '__main__':
